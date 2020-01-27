b8ae58111f6c9cc636eb585c9e7caa6b12ec7e6b
Simeon Reusch <simeon.reusch@desy.de>
2020-01-27 18:21:49 +0100

    Update saltfit.py

--- saltfit.py
+++ saltfit.py
@@ -13,7 +13,15 @@ m = marshal.MarshalAccess()
 # m.load_target_sources("Cosmology")
 m = m.load_local("Cosmology")
 
+
+_ALPHA_JLA_ = 0.141
+_ALPHA_JLA_UNC_ = 0.006
+_BETA_JLA_ = 3.101
+_BETA_JLA_UNC_ = 0.075
+
+
 class SaltFit():
+
 	def __init__(self, ztf_name, logger=None, **kwargs):
 		if logger is None:
 			logging.basicConfig(level = logging.INFO)
@@ -25,9 +33,7 @@ class SaltFit():
 		self.ra = m.target_sources.query('name == "{}"'.format(ztf_name))['ra'].values[0]
 		self.dec = m.target_sources.query('name == "{}"'.format(ztf_name))['dec'].values[0]
 		self.z = m.target_sources.query('name == "{}"'.format(ztf_name))['redshift'].values[0]
-		self.mwebv = self.get_mwebv(self)
 
-	@staticmethod
 	def modify_columns(self):
 		self.lightcurve.replace(['ZTF_r', 'ZTF_g', 'ZTF_i'], ['p48r', 'p48g', 'p48i'], inplace=True)
 		self.lightcurve.replace(['ZTF r', 'ZTF g', 'ZTF i'], ['p48r', 'p48g', 'p48i'], inplace=True)
@@ -41,50 +47,42 @@ class SaltFit():
 		self.lightcurve["Fratio"] = self.lightcurve.ampl / self.lightcurve.F0
 		self.lightcurve["Fratio_unc"] = np.sqrt( (self.lightcurve["ampl.err"] / self.lightcurve.F0)**2 + (self.lightcurve.ampl * self.lightcurve["F0.err"] / self.lightcurve.F0**2)**2 )
 
-	def load_ztf_filters(self):
-		bands = {'p48r' : 'data/ztfr_eff.txt', 'p48g' : 'data/ztfg_eff.txt'}
+	@staticmethod
+	def load_ztf_filters():
+		bands = {'p48r': 'data/ztfr_eff.dat', 'p48g': 'data/ztfg_eff.dat', 'p48i': 'data/ztfi_eff.dat'}
 		for bandname in bands.keys():
 			fname = bands[bandname]
 			b = np.loadtxt(fname)
 			band = sncosmo.Bandpass(b[:,0], b[:,1], name=bandname)
 			sncosmo.registry.register(band, force=True)
 
-	@staticmethod
-	def get_mwebv(self):
-		import sfdmap
-		dustmap = sfdmap.SFDMap()
-		return dustmap.ebv(self.ra, self.dec)
-
-	def fit(self, snt=4, **kwargs):
+	def fit(self, SNT=5, **kwargs):
 		from astropy.table import Table
 		from astropy.cosmology import Planck15 as cosmo 
-		self.snt = snt
+		self.SNT = SNT
 		dust = sncosmo.CCM89Dust()
-		self.modify_columns(self)
-		self.lightcurve = self.lightcurve.query('filter == ("p48g", "p48r") and chi2 > 0 and Fratio > (Fratio_unc * @self.snt)')
+		self.modify_columns()
+		self.lightcurve = self.lightcurve.query('chi2 > 0 and Fratio > (Fratio_unc * @self.SNT)')
 		lc_sncosmo = Table.from_pandas(self.lightcurve.query('filter == ("p48g", "p48r") and chi2 > 0')[['mjd', 'filter', 'flux', 'flux_err', 'zp', 'zpsys']])
 		salt_model = sncosmo.Model(source='salt2', effects = [dust], effect_names = ['mw'], effect_frames = ['obs'])
 		salt_model.set(z = self.z)
-		salt_model.set(mwebv  = self.mwebv)
+		salt_model.set(mwebv = dustmap.ebv(self.ra, self.dec))
 		self.load_ztf_filters()
 		try:
-			self.fitresult, self.fitted_model = sncosmo.fit_lc(lc_sncosmo, salt_model, ['t0', 'x0', 'x1', 'c'], phase_range=(-30., 50.), minsnr=self.snt)
+			self.fitresult, self.fitted_model = sncosmo.fit_lc(lc_sncosmo, salt_model, ['t0', 'x0', 'x1', 'c'], phase_range=(-30., 50.), minsnr=self.SNT)
+			ab = sncosmo.get_magsystem('ab')
+			flux_zp = ab.zpbandflux('p48g')
+			bandflux = self.fitted_model.bandflux(band = 'p48g', time = self.fitresult['parameters'][1], zpsys = 'ab')
+			peak_mag = ab.band_flux_to_mag(bandflux, 'p48g')
+			peak_abs_mag_for_comparison = self.fitted_model.source_peakabsmag(band = 'p48g', magsys = 'ab')
+			peak_abs_mag = peak_mag - cosmo.distmod(self.z).value
+			peak_abs_mag_corrected = peak_abs_mag + _ALPHA_JLA_*self.fitresult['parameters'][3] - _BETA_JLA_*self.fitresult['parameters'][4]
 			self.fitresult['name'] = self.ztf_name
-			self.fitresult['peak_mag'] = self.fitted_model.source_peakmag(band = 'p48g', magsys = 'ab')
-			self.fitresult['peak_abs_mag'] = self.fitted_model.source_peakabsmag(band = 'p48g', magsys = 'ab', cosmo = cosmo)
-			self.fitted_model.parameters[2] = self.fitted_model.parameters[2] + self.fitresult.errors['x0']
-			self.fitted_model.parameters[3] = self.fitted_model.parameters[3] + self.fitresult.errors['x1']
-			self.fitted_model.parameters[4] = self.fitted_model.parameters[4] + self.fitresult.errors['c']
-			self.fitresult['peak_mag_upper'] = self.fitted_model.source_peakmag(band = 'p48g', magsys = 'ab')
-			self.fitresult['peak_abs_mag_upper'] = self.fitted_model.source_peakabsmag(band = 'p48g', magsys = 'ab', cosmo = cosmo)
-			self.fitted_model.parameters[2] = self.fitted_model.parameters[2] - 2*self.fitresult.errors['x0']
-			self.fitted_model.parameters[3] = self.fitted_model.parameters[3] - 2*self.fitresult.errors['x1']
-			self.fitted_model.parameters[4] = self.fitted_model.parameters[4] - 2*self.fitresult.errors['c']
-			self.fitresult['peak_mag_lower'] = self.fitted_model.source_peakmag(band = 'p48g', magsys = 'ab')
-			self.fitresult['peak_abs_mag_lower'] = self.fitted_model.source_peakabsmag(band = 'p48g', magsys = 'ab', cosmo = cosmo)
-			self.fitted_model.parameters[2] = self.fitted_model.parameters[2] + self.fitresult.errors['x0']
-			self.fitted_model.parameters[3] = self.fitted_model.parameters[3] + self.fitresult.errors['x1']
-			self.fitted_model.parameters[4] = self.fitted_model.parameters[4] + self.fitresult.errors['c']
+			self.fitresult['peak_mag'] = peak_mag
+			self.fitresult['peak_abs_mag'] = peak_abs_mag
+			self.fitresult['peak_abs_mag_for_comparison'] = peak_abs_mag_for_comparison
+			self.fitresult['peak_abs_mag_corrected'] = peak_abs_mag_corrected
+	 
 			import matplotlib.pyplot as plt
 			fig = sncosmo.plot_lc(lc_sncosmo, model=self.fitted_model, errors=self.fitresult.errors, figtext=str(self.ztf_name))
 			plotdir = os.path.join(LOCALDATA, 'SALT')
@@ -104,5 +102,5 @@ class SaltFit():
 
 def fit_salt(ztf_name, logger=None):
 	saltfit = SaltFit(ztf_name, plot=True, logger=logger)
-	saltfit.fit(snt=4)
+	saltfit.fit(SNT=5)
 	return saltfit.fitresult, saltfit.fitted_model
