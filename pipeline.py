7536fed50fdca84415d4cb2c55b53739f96a2c1f
simeonreusch <simeon.reusch@desy.de>
2020-01-29 15:59:48 +0100

    s

--- pipeline.py
+++ pipeline.py
@@ -156,7 +156,7 @@ class ForcedPhotometryPipeline():
 		fp.load_filepathes(filecheck=False)
 		print('{} Fitting PSF'.format(ztf_name))
 		import matplotlib.pyplot as plt
-		fp.run_forcefit(verbose=True)
+		fp.run_forcefit(verbose=False)
 		fig = plt.figure(dpi = 300)
 		ax = fig.add_subplot(111)
 		fp.show_lc(ax=ax)
