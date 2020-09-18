6242e9a208e8fc449217b0215c932bdc643f241e
simeonreusch <simeon.reusch@desy.de>
2020-09-18 17:27:01 +0200

    remove print command

--- ztffps/plot.py
+++ ztffps/plot.py
@@ -100,7 +100,6 @@ def plot_lightcurve(
     lc.sort_values(by=["obsmjd"], inplace=True)
     lc.reset_index(inplace=True)
     lc.drop(columns=["index"], inplace=True)
-    print(lc.obsmjd)
 
     # Save this version of the dataframe for later analysis (and to be sent by mail)
     lc.to_csv(os.path.join(lc_plotted_dir, f"{name}_SNT_{snt}.csv"))
@@ -125,7 +124,6 @@ def plot_lightcurve(
         ]
         alert_df = alert_df.reset_index()
         alert_df.drop(columns=["index"], inplace=True)
-        print(alert_df)
         alert_df.to_csv(os.path.join(lc_plotted_dir, f"{name}_alert.csv"))
         alert_g = alert_df.query("filter_id == 1")
         alert_r = alert_df.query("filter_id == 2")
