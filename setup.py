ac545c48536ea3c541bfb0a5697a237f73865e0e
simeonreusch <simeon.reusch@desy.de>
2022-07-12 12:51:06 +0200

    rewrote dependencies

--- setup.py
+++ setup.py
@@ -1,165 +1,62 @@
-DESCRIPTION = "ZTF Forced Photometry Pipeline"
-LONG_DESCRIPTION = """Provides a Forced Photometry Pipeline for ZTF based on ztfquery and ztflc, needs IPAC as well as Fritz or AMPEL access."""
+import setuptools
 
-DISTNAME = "ztffps"
-AUTHOR = "Simeon Reusch"
-MAINTAINER = "Simeon Reusch"
-MAINTAINER_EMAIL = "simeon.reusch@desy.de"
-URL = "https://github.com/simeonreusch/modelSED/"
-LICENSE = "BSD (3-clause)"
-DOWNLOAD_URL = "https://github.com/simeonreusch/ztffps/archive/v1.0.3.tar.gz"
-VERSION = "1.0.3"
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-try:
-    from setuptools import setup, find_packages
-
-    _has_setuptools = True
-except ImportError:
-    from distutils.core import setup
-
-    _has_setuptools = False
-
-
-def check_dependencies():
-    install_requires = []
-
-    # Make sure dependencies exist.
-    try:
-        import astropy
-    except ImportError:
-        install_requires.append("astropy")
-    try:
-        import numpy
-    except ImportError:
-        install_requires.append("numpy")
-    try:
-        import sncosmo
-    except ImportError:
-        install_requires.append("sncosmo")
-    try:
-        import extinction
-    except ImportError:
-        install_requires.append("extinction")
-    try:
-        import pandas
-    except ImportError:
-        install_requires.append("pandas")
-    try:
-        import matplotlib
-    except ImportError:
-        install_requires.append("matplotlib")
-    try:
-        import scipy
-    except ImportError:
-        install_requires.append("scipy")
-    try:
-        import slackclient
-    except ImportError:
-        install_requires.append("slackclient")
-    try:
-        import sqlalchemy
-    except ImportError:
-        install_requires.append("sqlalchemy")
-    try:
-        import requests
-    except ImportError:
-        install_requires.append("requests")
-    try:
-        import lxml
-    except ImportError:
-        install_requires.append("lxml")
-    try:
-        import html5lib
-    except ImportError:
-        install_requires.append("html5lib")
-    try:
-        import bs4
-    except ImportError:
-        install_requires.append("bs4")
-    try:
-        import iminuit
-    except ImportError:
-        install_requires.append("iminuit<2")
-    try:
-        import sfdmap
-    except ImportError:
-        install_requires.append("sfdmap")
-    try:
-        import pymongo
-    except ImportError:
-        install_requires.append("pymongo")
-    try:
-        import fastapi
-    except ImportError:
-        install_requires.append("fastapi")
-    try:
-        import uvicorn
-    except ImportError:
-        install_requires.append("uvicorn")
-    try:
-        import keyring
-    except ImportError:
-        install_requires.append("keyring")
-    try:
-        import ztflc
-    except ImportError:
-        install_requires.append("ztflc")
-    try:
-        import ztfquery
-    except ImportError:
-        install_requires.append("ztfquery")
-    try:
-        import tqdm
-    except ImportError:
-        install_requires.append("tqdm")
-
-    return install_requires
-
-
-if __name__ == "__main__":
-
-    install_requires = check_dependencies()
-
-    if _has_setuptools:
-        packages = find_packages()
-        print(packages)
-    else:
-        packages = ["ztffps"]
-
-    setup(
-        name=DISTNAME,
-        author=AUTHOR,
-        author_email=MAINTAINER_EMAIL,
-        maintainer=MAINTAINER,
-        maintainer_email=MAINTAINER_EMAIL,
-        description=DESCRIPTION,
-        long_description=LONG_DESCRIPTION,
-        license=LICENSE,
-        url=URL,
-        version=VERSION,
-        download_url=DOWNLOAD_URL,
-        install_requires=install_requires,
-        scripts=[
-            "forcedphotometry",
-            "fps_slackbot",
-            "fps_start_slackbot",
-            "fps_slackbot_spawn_screen_session",
-            "fps_slackbot_spawn_screen_session_debug",
-        ],
-        packages=packages,
-        package_data={
-            "ztffps": [
-                "data/*.dat",
-                "data/*.jpg",
-            ]
-        },
-        classifiers=[
-            "Intended Audience :: Science/Research",
-            "Programming Language :: Python :: 3.7",
-            "License :: OSI Approved :: BSD License",
-            "Topic :: Scientific/Engineering :: Astronomy",
-            "Operating System :: POSIX",
-            "Operating System :: Unix",
-            "Operating System :: MacOS",
-        ],
-    )
+setuptools.setup(
+    name="nuztf",
+    version="1.0.3",
+    author="Simeon Reusch",
+    author_email="simeon.reusch@desy.de",
+    description="ZTF Forced Photometry Pipeline",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="BSD (3-clause)",
+    keywords="astrophysics astronomy ZTF",
+    url="https://github.com/simeonreusch/fpbot",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+    ],
+    python_requires=">=3.8.0",
+    package_data={
+        "ztffps": [
+            "data/*.dat",
+            "data/*.jpg",
+        ]
+    },
+    scripts=[
+        "forcedphotometry",
+        "fps_slackbot",
+        "fps_start_slackbot",
+        "fps_slackbot_spawn_screen_session",
+        "fps_slackbot_spawn_screen_session_debug",
+    ],
+    install_requires=[
+        "astropy",
+        "numpy",
+        "sncosmo",
+        "extinction",
+        "pandas",
+        "matplotlib",
+        "scipy",
+        "slackclient",
+        "sqlalchemy",
+        "requests",
+        "lxml",
+        "html5lib",
+        "bs4",
+        "iminuit<2",
+        "sfdmap",
+        "pymongo",
+        "fastapi",
+        "uvicorn",
+        "keyring",
+        "ztflc",
+        "ztfquery",
+        "tqdm",
+    ],
+)
