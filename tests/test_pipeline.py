69032fc2535f9f9912f3f20f09a18be186843bfa
simeonreusch <simeon.reusch@desy.de>
2022-08-11 12:21:50 +0200

    test checks now if file has been written

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import logging
+import logging, os
 import unittest
 import ztfquery
 
-from fpbot.pipeline import ForcedPhotometryPipeline
+from fpbot.pipeline import ForcedPhotometryPipeline, FORCEPHOTODATA
 
 
 class TestPipeline(unittest.TestCase):
@@ -22,8 +22,10 @@ class TestPipeline(unittest.TestCase):
 
         jdmin = 2459761.50000
 
+        ztf_id = "ZTF19aatubsj"
+
         pl = ForcedPhotometryPipeline(
-            file_or_name="ZTF19aatubsj",
+            file_or_name=ztf_id,
             jdmin=jdmin,
             jdmax=jdmin + 3,
             ampel=True,
@@ -32,6 +34,14 @@ class TestPipeline(unittest.TestCase):
         pl.download()
         pl.psffit()
         pl.plot()
+
+        df_filepath = os.path.join(FORCEPHOTODATA, ztf_id + ".csv")
+        plot_filepath = os.path.join(
+            FORCEPHOTODATA, "plots", "images", ztf_id + "_SNT_5.0.png"
+        )
+
+        self.assertTrue(os.path.isfile(df_filepath))
+        self.assertTrue(os.path.isfile(plot_filepath))
 
 
 if __name__ == "__main__":
