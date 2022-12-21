47d0054b2b5a89a3441c4c91554e262d3e6fb321
simeonreusch <simeon.reusch@desy.de>
2022-12-21 14:14:54 +0100

    logging

--- delete_from_db.py
+++ delete_from_db.py
@@ -22,62 +22,64 @@ else:
 
 METADATA_COLL = MONGO_DB.metadata
 
+if __name__ == "__main__":
+    logging.basicConfig()
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
 
-def delete_from_db(ztf_names):
-    for name in ztf_names:
-        METADATA_COLL.delete_one({"_id": name})
+    def delete_from_db(ztf_names):
+        for name in ztf_names:
+            METADATA_COLL.delete_one({"_id": name})
+            logger.info(f"deleted {ztf_names} from local database")
 
+    def is_ztf_name(name):
+        """
+        Checks if a string adheres to the ZTF naming scheme
+        """
+        return re.match("^ZTF[1-2]\d[a-z]{7}$", name)
 
-def is_ztf_name(name):
-    """
-    Checks if a string adheres to the ZTF naming scheme
-    """
-    return re.match("^ZTF[1-2]\d[a-z]{7}$", name)
+    def use_if_ztf(file_or_name):
+        """
+        Checks if name argument is a ZTF name (must fit ZTF naming convention),
+        an ascii file containing ZTF names (1 per line) in the program
+        directory or an arbitrary name if -radec argument to the
+        pipeline class
+        """
+        errormessage = "\nYou have to provide a either a ZTF name (a string adhering to the ZTF naming scheme), an ascii file containing ZTF names (1 per line) in the same directory or an arbitrary name if using the radec option.\n"
 
+        if is_ztf_name(file_or_name):
+            object_list = [file_or_name]
+        else:
+            object_list = []
+            try:
+                file = open(f"{file_or_name}", "r")
+                lines = file.read().splitlines()
+                for line in lines:
+                    if is_ztf_name(line):
+                        object_list.append(line)
+            except:
+                object_list.append(file_or_name)
 
-def use_if_ztf(file_or_name):
-    """
-    Checks if name argument is a ZTF name (must fit ZTF naming convention),
-    an ascii file containing ZTF names (1 per line) in the program
-    directory or an arbitrary name if -radec argument to the
-    pipeline class
-    """
-    errormessage = "\nYou have to provide a either a ZTF name (a string adhering to the ZTF naming scheme), an ascii file containing ZTF names (1 per line) in the same directory or an arbitrary name if using the radec option.\n"
+        return object_list
 
-    if is_ztf_name(file_or_name):
-        object_list = [file_or_name]
+    parser = argparse.ArgumentParser(
+        description="Used to obtain forced photometry for selection of SNe in parallel"
+    )
+
+    parser.add_argument(
+        "name",
+        type=str,
+        help='Provide a ZTF name (e.g. "ZTF19aaelulu") or a .txt-file containing a list of ZTF names',
+    )
+
+    commandline_args = parser.parse_args()
+    file_or_name = commandline_args.name
+
+    if isinstance(file_or_name, str):
+        object_list = use_if_ztf(file_or_name)
+    elif isinstance(file_or_name, list):
+        object_list = file_or_name
     else:
-        object_list = []
-        try:
-            file = open(f"{file_or_name}", "r")
-            lines = file.read().splitlines()
-            for line in lines:
-                if is_ztf_name(line):
-                    object_list.append(line)
-        except:
-            object_list.append(file_or_name)
+        raise TypeError
 
-    return object_list
-
-
-parser = argparse.ArgumentParser(
-    description="Used to obtain forced photometry for selection of SNe in parallel"
-)
-
-parser.add_argument(
-    "name",
-    type=str,
-    help='Provide a ZTF name (e.g. "ZTF19aaelulu") or a .txt-file containing a list of ZTF names',
-)
-
-commandline_args = parser.parse_args()
-file_or_name = commandline_args.name
-
-if isinstance(file_or_name, str):
-    object_list = use_if_ztf(file_or_name)
-elif isinstance(file_or_name, list):
-    object_list = file_or_name
-else:
-    raise TypeError
-
-delete_from_db(object_list)
+    delete_from_db(object_list)
